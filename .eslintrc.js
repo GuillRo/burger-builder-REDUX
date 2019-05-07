69d043ca16808aacc83f3398491e38aa68c5ffe8
Guillaume Rossi <guillaum.ro@gmail.com>
2019-05-07 16:28:45 +0200

    burgers is displayed but hardcoded

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "airbnb",
+    "extends": ["standard", "react-app"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+function App () {
   return (
     <div className="App">
-      Hello !
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
