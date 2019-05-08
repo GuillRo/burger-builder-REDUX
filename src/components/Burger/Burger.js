1ba2e02d0cd4dcea8021d72a2832e5c62733b68f
Guillaume Rossi <guillaum.ro@gmail.com>
2019-05-08 11:53:31 +0200

    controls unmapped

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css'
 
 const burger = (props) => {
+  let arrayIngredients = Object.keys(props.ingredients)
+    .map(ingrdKey => {
+      return [...Array(props.ingredients[ingrdKey])]
+        .map((_, i) => {
+          return <BurgerIngredient key={ingrdKey + i} type={ingrdKey} />
+        })
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+
+  if (arrayIngredients.length === 0) {
+    arrayIngredients = <p className="Burger">Please start adding ingredients</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {arrayIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
