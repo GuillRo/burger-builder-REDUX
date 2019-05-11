a2b0490166274261f5b58199437709d4405e8bf5
Guillaume Rossi <guillaum.ro@gmail.com>
2019-05-11 11:16:26 +0200

    interesting bug where the state is not updated and you can order only after two ingredients are added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,17 +5,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css'
 
 const burger = (props) => {
-  console.log(props.ingredients)
+  // V3, maintenant ça suit toujours le meme pattern, c'est plus joli...
+  // Si j'ai le temps j'essaierai une V4 ou le truc est toujours ajouté en dernier ou un truc du genre...
+
   let arrayIngredients = []
   let arrayIngredientsTemp = Object.entries(props.ingredients)
-  arrayIngredientsTemp.forEach(arr => {
-    for (let i = 0; i < arr[1]; i++) {
-      arrayIngredients.push(<BurgerIngredient key={shortid.generate()} type={arr[0]} />)
-    }
-  })
+  let completed = false
 
-  // ça en dessous c'est un pattern de la vidéo qui fait la même chose que plus haut
-  // mais en incompréhensible... ça vaut le coup de regarder, c'est ptet + rapide
+  const fillArrayIng = (arr) => {
+    if (arr[1] > 0) {
+      arr[1] -= 1
+      arrayIngredients.push(<BurgerIngredient key={shortid.generate()} type={arr[0]} />)
+      completed = false
+    }
+  }
+
+  while (!completed) {
+    completed = true
+    arrayIngredientsTemp.forEach(arr => {
+      fillArrayIng(arr)
+    })
+  }
+
+  // V1, exemple de la vidéo, assez incompréhensible mais bon... peut être intéressant de piger
 
   // let arrayIngredients = Object.keys(props.ingredients)
   //   .map(ingrdKey => {
@@ -27,6 +39,18 @@ const burger = (props) => {
   //   .reduce((arr, el) => {
   //     return arr.concat(el)
   //   }, [])
+
+  // V2, ma version a moi, fais la meme chose que v1 mais en compréhensible.
+  // Par contre les meat sont ensemble, les cheese aussi, c'est pas super beau...
+  // c'est pareil pour la version de la vidéo mais je vais tenter d'améliorer ça
+
+  // let arrayIngredients = []
+  // let arrayIngredientsTemp = Object.entries(props.ingredients)
+  // arrayIngredientsTemp.forEach(arr => {
+  //   for (let i = 0; i < arr[1]; i++) {
+  //     arrayIngredients.push(<BurgerIngredient key={shortid.generate()} type={arr[0]} />)
+  //   }
+  // })
 
   if (arrayIngredients.length === 0) {
     arrayIngredients = <p className="Burger">Please start adding ingredients</p>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 0
+    totalPrice: 0,
+    purchasable: false
+  }
+
+  updatePuchaseState = () => {
+    const ingredients = {
+      ...this.state.ingredients
+    }
+    const sum = Object.keys(ingredients)
+      .map(ing => ingredients[ing])
+      .reduce((total, current) => { return total + current }, 0)
+
+    this.setState({ purchasable: sum > 0 })
   }
 
   addIngredientHandler = (type) => {
@@ -33,17 +45,46 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice
     const newPrice = oldPrice + priceAddition
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePuchaseState()
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    if (oldCount <= 0) {
+      return
+    }
+    const updatedCount = oldCount - 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
 
+    updatedIngredients[type] = updatedCount
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePuchaseState()
   }
 
-  render () {
+  render() {
+
+    const disableInfo = {
+      ...this.state.ingredients
+    }
+    for (let key in disableInfo) {
+      disableInfo[key] = disableInfo[key] <= 0
+    }
+
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disableInfo}
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
+        />
       </>
     )
   }
